b3229f7e25133f2b6939d8452b4e415e9fb1766c
RudyBekker <rudy@superagency.io>
2021-02-27 21:55:07 +0200

    update

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "607e8cee0442e72a7deb";
+/******/ 	var hotCurrentHash = "b1c7b4cc1d5592becbd7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
